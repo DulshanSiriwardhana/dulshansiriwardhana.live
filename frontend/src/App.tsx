b3b9ef0faefe1ac174a7b0c738ffe9a5853a3d78
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-10-25 01:09:44 +0530

    frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return(
+    <div className='w-full flex items-center justify-center bg-amber-200 border text-center'>
+      dulshansiriwardhana.live
+    </div>
+  )
+}
+
+export default App
