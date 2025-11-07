9daebc8a00eafe2f3f310efe717117815ba5750a
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-11-08 00:18:49 +0530

    timeout added for terminal

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
 import Terminal from "../components/Terminal";
+import LandingPage from "./LandingPgae";
 
 const HomePage = () => {
+  const [isTerminalOpen, setIsTerminalOpen] = useState(true);
   const commands = [
     "echo Hello from the homepage!",
     "ls -la",
@@ -12,13 +15,23 @@ const HomePage = () => {
     console.log("User typed:", input);
   };
 
+  useEffect(()=>{
+    setTimeout(()=>setIsTerminalOpen(false), 10000);
+  },[]);
+
   return (
     <div className="w-full h-full flex items-center justify-center p-4">
-      <Terminal
-        commands={commands}
-        enableUserTyping={true}
-        onUserInput={handleUserInput}
-      />
+      {
+        isTerminalOpen ? (
+        <Terminal
+          commands={commands}
+          enableUserTyping={true}
+          onUserInput={handleUserInput}
+        />
+        ) : (
+          <LandingPage/>
+        )
+      }
     </div>
   );
 };
---
+++ frontend/src/pages/LandingPgae.tsx
@@ -0,0 +1,9 @@
+const LandingPage=()=>{
+    return(
+        <div>
+            Hi
+        </div>
+    )
+}
+
+export default LandingPage;
